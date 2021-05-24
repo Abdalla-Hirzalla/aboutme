645bd7b1a56cf8177f12ed2b4af0f30df1856309
Abdalla-Hirzalla <a.hirzalla@hotmail.com>
2021-05-24 16:27:07 +0300

    add new files and qustions

---
+++ js/app.js
@@ -0,0 +1,89 @@
+'use strict'
+
+let userName=prompt('what\s your name ?');
+alert('welcome to my website' + ' '+userName );
+console.log(userName);
+
+let about1=prompt('do you think i love sports ?');
+
+if (about1 === 'yes'|| about1 === 'y') {
+    alert('you are correct i love every sport in the world');
+    console.log(about1);
+
+}else if(about1 === 'no'|| about1 === 'n' ){
+    alert(' you should know i like every sport in the world');
+    console.log(about1);
+}
+else{
+    alert('Answer with yes/y or no/n please' );
+   console.log(about1);
+
+}
+
+let about2=prompt('do you think that i support Realmadrid team ?');
+
+if (about2 === 'yes'|| about2 === 'y') {
+    alert('you are correct i\'m a big fan for realmadrid');
+    console.log(about2);
+
+}else if(about2 === 'no'|| about2 === 'n' ){
+    alert(' you must know my best team is realmadrid in football world');
+    console.log(about2);
+}
+else{
+    alert('Answer with yes/y or no/n please' );
+   console.log(about2);}
+
+
+  
+let about3=prompt('do you think i love basketball same as football?');
+
+if (about3 === 'yes'|| about3 === 'y') {
+    alert('you are correct my favourite sports is football and basketball');
+     console.log(about3);
+
+}else if(about3 === 'no'|| about3 === 'n' ){
+    alert('my favourite sports and always watch basketball and football and i love them both');
+     console.log(about3);
+}
+else{
+    alert('Answer with yes/y or no/n please' );
+    console.log(about3);
+}
+
+
+
+   let about4=prompt('do you think Gulf is the most boring game to me ?');
+
+   if (about4 === 'yes'|| about4 === 'y') {
+       alert('you are correct i didn\'t see Gulf a intersting game to watch');
+       // console.log(about4);
+   
+   }else if(about4 === 'no'|| about4 === 'n' ){
+       alert(' you must know the most game i Hated is Gulf');
+       // console.log(about4);
+   }
+   else{
+       alert('Answer with yes/y or no/n please' );
+      // console.log(about4);
+    }
+    
+
+
+      
+   let about5=prompt('do you think i watch other things like Movies ?');
+
+   if (about5 === 'yes'|| about5 === 'y') {
+       alert('of course i watch movies and series not all my life related to sports');
+       // console.log(about5);
+
+   }else if(about5 === 'no'|| about5 === 'n' ){
+       alert('No one in his life can only watching one thing like sport so i watch beside sport. Movies and other things');
+      //  console.log(about5);
+   }
+   else{
+       alert('Answer with yes/y or no/n please' );
+      //  console.log(about5);
+    }
+    
+      alert('Thank you for visiting our website and answer the qustions' + ' '+userName );
