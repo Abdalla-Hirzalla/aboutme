ba0dd150b52c51f09ab745bc0ce35bdd9ff0bb2e
Abdalla-Hirzalla <a.hirzalla@hotmail.com>
2021-05-27 20:57:56 +0300

    add the final touch

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ let counter=0
 
 let userName=prompt('what\s your name ?');
 alert('welcome to my website' + ' '+userName );
-console.log(userName);
+
 
 function about1() {
     
@@ -12,17 +12,17 @@ let about1=prompt('do you think i love sports ?');
 about1=about1.toLowerCase();
 if (about1 === 'yes'|| about1 === 'y') {
     alert('you are correct i love every sport in the world');
-    console.log(about1);
+   
     counter++;
    
 }else if(about1 === 'no'|| about1 === 'n' ){
     alert(' you should know i like every sport in the world');
-    console.log(about1);
+    
     
 }
 else{
     alert('Answer with yes/y or no/n please' );
-   console.log(about1);
+   
   
 }
 }
@@ -34,16 +34,17 @@ let about2=prompt('do you think that i support Realmadrid team ?');
 about2=about2.toLowerCase();
 if (about2 === 'yes'|| about2 === 'y') {
     alert('you are correct i\'m a big fan for realmadrid');
-    console.log(about2);
+    
     counter++;
 
 }else if(about2 === 'no'|| about2 === 'n' ){
     alert(' you must know my best team is realmadrid in football world');
-    console.log(about2);
+   
 }
 else{
     alert('Answer with yes/y or no/n please' );
-   console.log(about2);}
+   
+}
 }
 about2()
 
@@ -52,33 +53,34 @@ let about3=prompt('do you think i love basketball same as football?');
 about3=about3.toLowerCase();
 if (about3 === 'yes'|| about3 === 'y') {
     alert('you are correct my favourite sports is football and basketball');
-    console.log(about3);
+    
     counter++;
 }else if(about3 === 'no'|| about3 === 'n' ){
     alert('my favourite sports and always watch basketball and football and i love them both');
-     console.log(about3);
+    
 }
 else{
     alert('Answer with yes/y or no/n please' );
-    console.log(about3);
+  
 }
-}about3()
+}
+about3()
 
 function about4() {
    let about4=prompt('do you think Gulf is the most boring game to me ?');
    about4=about4.toLowerCase();
    if (about4 === 'yes'|| about4 === 'y') {
        alert('you are correct i didn\'t see Gulf a intersting game to watch');
-       console.log(about4);
+   
        counter++;
    
    }else if(about4 === 'no'|| about4 === 'n' ){
        alert(' you must know the most game i Hated is Gulf');
-        console.log(about4);
+        
    }
    else{
        alert('Answer with yes/y or no/n please' );
-      console.log(about4);
+     
     }
 }about4()
 
@@ -88,16 +90,16 @@ function about5() {
    about5=about5.toLowerCase();
    if (about5 === 'yes'|| about5 === 'y') {
        alert('of course i watch movies and series not all my life related to sports');
-       console.log(about5);
+       
        counter++;
 
    }else if(about5 === 'no'|| about5 === 'n' ){
        alert('No one in his life can only watching one thing like sport so i watch beside sport. Movies and other things');
-        console.log(about5);
+      
    }
    else{
        alert('Answer with yes/y or no/n please' );
-        console.log(about5);
+  
     }
     
       alert('Thank you for visiting our website and answer the qustions' + ' '+userName );
@@ -110,7 +112,7 @@ let about6=Number(prompt('Choose a number between 1 to 10 ?' ));
 for (let i = 1; i <= 4;i++){
 if (about6 === 7) {
     alert('you are correct');
-    console.log(about6);
+    
     counter++
     
 
@@ -119,17 +121,17 @@ if (about6 === 7) {
 
 } else if (about6 >7 && about6 <= 10){
     about6=Number(prompt('too High ?' ));
-    console.log(about6);
+    
 
 } else if (about6 >=1 && about6 < 7 ){
 about6=Number(prompt('too low?' ));
-console.log(about6);
+
 
 } else if (about6 < 1 && about6 > 10){
     about6=Number(prompt('please select between 1 to 10 only'));
-    console.log(about6)
-   
-  }
+
+}
+
   
 
 if (i===3){
@@ -140,7 +142,8 @@ alert('the correct answer is 7')
 
 
 }
-}about6() 
+}
+about6();
 
 function about7() {
 let traditional=['maqloba','mansaf','kabsa','dwali'];
@@ -155,7 +158,7 @@ for(let i = 0; i<=5; i++){
       if (about7===traditional[x]){
        alert('you are correct i love these traditional food');
        flag=true;
-       console.log(about7);
+    
        counter++;
      break;}
      
@@ -166,18 +169,18 @@ for(let i = 0; i<=5; i++){
     }else{
        alert('not correct');
        about7= prompt('try again ?' )
-       console.log(about7);
+      
     }
 
 if (i===4) {
     alert('you Are out of attempts');
     alert('The correct answer is' +' '+ traditional);
-    console.log(about7);
+   
 break;
 }
-
 }
-}about7()
+}
+about7();
 
 alert('your total score of 7 quastion is'+' ' +counter);
 
